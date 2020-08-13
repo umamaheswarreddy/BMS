919de0e74ff72b72947b4dcfc2dbec246607b406
umamaheswar reddy <35299489+umamaheswarreddy@users.noreply.github.com>
2020-08-13 20:41:39 +0530

    zuul

---
+++ Zuul-Gate-Way/src/main/java/ZuulFilters/ZuulErrorLoggingFilter.java
@@ -0,0 +1,35 @@
+package ZuulFilters;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class ZuulErrorLoggingFilter extends ZuulFilter {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public String filterType() {
+        return "error";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return RequestContext.getCurrentContext().getThrowable() != null;
+    }
+
+    @Override
+    public Object run() {
+        Throwable throwable = RequestContext.getCurrentContext().getThrowable();
+        logger.error("Exception was thrown in filters: ", throwable);
+        return null;
+    }
+}
